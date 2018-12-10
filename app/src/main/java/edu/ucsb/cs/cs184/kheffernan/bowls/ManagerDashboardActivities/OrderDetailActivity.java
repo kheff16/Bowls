bcf0871280691de2f4fdfc44febe4182c77b60d3
Arielle Robles <acr@umail.ucsb.edu>
2018-12-09 18:18:43 -0800

    implemented functionality for Move To Next Stage button

--- app/src/main/java/edu/ucsb/cs/cs184/kheffernan/bowls/ManagerDashboardActivities/OrderDetailActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/kheffernan/bowls/ManagerDashboardActivities/OrderDetailActivity.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.ucsb.cs.cs184.kheffernan.bowls.BowlsFirebaseInterface.BowlsFirebase;
 import edu.ucsb.cs.cs184.kheffernan.bowls.BowlsFirebaseInterface.BowlsFirebaseCallback;
 import edu.ucsb.cs.cs184.kheffernan.bowls.BowlsLocalObjects.BowlsUser;
 import edu.ucsb.cs.cs184.kheffernan.bowls.BowlsLocalObjects.Order;
 import edu.ucsb.cs.cs184.kheffernan.bowls.R;
+
+import static edu.ucsb.cs.cs184.kheffernan.bowls.Utilities.BowlsConstants.ORDER_STATUS_COMPLETED;
+import static edu.ucsb.cs.cs184.kheffernan.bowls.Utilities.BowlsConstants.ORDER_STATUS_CREATED;
+import static edu.ucsb.cs.cs184.kheffernan.bowls.Utilities.BowlsConstants.ORDER_STATUS_IN_PROGRESS;
+import static edu.ucsb.cs.cs184.kheffernan.bowls.Utilities.BowlsConstants.ORDER_STATUS_READY_FOR_PICK_UP;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -36,7 +43,6 @@ public class OrderDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
 
-        moveToNextBtn = (Button) findViewById(R.id.move_to_next_stage_btn);
 
         bowlsFirebase = new BowlsFirebase();
         orderOwner = new BowlsUser();
@@ -47,7 +53,7 @@ public class OrderDetailActivity extends AppCompatActivity {
 
         // Get intent and extras
         Intent intent = getIntent();
-        String orderID = intent.getStringExtra("orderID");
+        orderID = intent.getStringExtra("orderID");
 
         //get order using info from intent
         bowlsFirebase.getOrder(orderID, new BowlsFirebaseCallback<Order>() {
@@ -64,8 +70,55 @@ public class OrderDetailActivity extends AppCompatActivity {
             }
         });
 
+        moveToNextBtn = (Button) findViewById(R.id.move_to_next_stage_button);
+        moveToNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onMoveNextBtnClicked();
+            }
+        });
+
 
     }
+
+    private void onMoveNextBtnClicked(){
+
+        bowlsFirebase.getOrder(orderID, new BowlsFirebaseCallback<Order>() {
+            @Override
+            public void callback(Order data) {
+                if (data!=null){
+                    order=data;
+                    switch (order.getOrderStatus()){
+                        case ORDER_STATUS_CREATED:{
+                            order.setOrderStatus(ORDER_STATUS_IN_PROGRESS);
+                            Log.d("OrderDetailActivity", "setting order status from created to in prog");
+                            bowlsFirebase.updateorder(orderID, order);
+                            break;
+                        }
+                        case ORDER_STATUS_IN_PROGRESS:{
+                            order.setOrderStatus(ORDER_STATUS_READY_FOR_PICK_UP);
+                            bowlsFirebase.updateorder(orderID, order);
+                            Log.d("OrderDetailActivity", "setting order status from in prog to ready for pickup");
+
+                            break;
+                        }
+                        case ORDER_STATUS_READY_FOR_PICK_UP:{
+                            order.setOrderStatus(ORDER_STATUS_COMPLETED);
+                            bowlsFirebase.updateorder(orderID, order);
+                            Log.d("OrderDetailActivity", "setting order status from ready for pickup to in completed");
+
+                            break;
+                        }
+                        default:{
+                            Log.d("OrderDetailActivity", "default case, order is complete");
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
 
 
 }
